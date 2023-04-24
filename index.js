043b86499236095a19ef2a4f2c6f574112458a18
AndyFreeL <andy00freelance@gmail.com>
2023-04-24 15:29:42 +0700

    useCreateIndex: true

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api/files', fileRouter)
 const start = async () => {
 
   try {
-    await mongoose.connect(config.get("dbUrl"), {useNewUrlParser:true,useUnifiedTopology: true})
+    await mongoose.connect(config.get("dbUrl"), {useNewUrlParser:true,useCreateIndex: true, useUnifiedTopology: true})
 
     app.listen(PORT, () => {
       console.log('Server is running', PORT)
